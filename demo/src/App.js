4dfef1a5a24a6513c6acd8d2d28688a3dfa13d6b
Dominik_Urbanetz_TBZ <dominik.urbanetz@edu.tbz.ch>
2022-07-05 15:59:20 +0200

    Update

--- demo/src/App.js
+++ demo/src/App.js
@@ -5,8 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <iframe src="https://discord.com/widget?id=905092436081115147&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" class="discordtest"></iframe>
+        <h1>Welcome on our discord Page</h1>
       </header>
+      <div class="content-box">
+        <iframe src="https://discord.com/widget?id=905092436081115147&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-scripts" class="discordtest "></iframe>
+      </div>
+
     </div>
   );
 }
