8e4ac6b4bc6878a9be68c9fa2af6ff243c6baa6e
Dominik_Urbanetz_TBZ <dominik.urbanetz@edu.tbz.ch>
2022-07-05 10:50:13 +0200

    test

---
+++ demo/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <iframe src="https://discord.com/widget?id=905092436081115147&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" class="discordtest"></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
