48957c3bab158bcd5fd5eca3ebda0e701a4119f7
Dominik_Urbanetz_TBZ <dominik.urbanetz@edu.tbz.ch>
2022-07-05 11:19:55 +0200

    infos

---
+++ start_infos.bat
@@ -0,0 +1,13 @@
+echo To run app  go to reactxnode/ with powershell 
+echo de demo
+echo npm start
+
+echo To run app  go to reactxnode/ with powershell
+echo npm run dev
+
+echo To open new poershell on same path type following command
+echo Start-Process powershell.exe
+
+
+
+echo on DEPLOY ERROR, commit to git
